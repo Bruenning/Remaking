7c02405e0fd68a7f15e9634f1f2759de366004fe
Bruenning <gui.h.bruenning@gmail.com>
2021-03-20 21:31:56 -0300

    aprendendo VUE

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <comments />
+  </div>
+</template>
+
+<script>
+import Comments from "./components/Comments.vue";
+
+export default {
+  name: "App",
+  components: {
+    Comments,
+  },
+};
+</script>
+
+<style></style>
